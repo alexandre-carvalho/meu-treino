3fcccd2a33eef8d600edff7e7fa834aa4aef9ddb
Alexandre Carvalho <alexandre.carvalho@opah.com.br>
2022-08-28 15:08:20 -0300

    Initial configs

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,47 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: [
+      'babel-preset-expo',
+      'module:metro-react-native-babel-preset',
+      '@babel/preset-typescript',
+    ],
+
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          envName: 'NODE_ENV',
+          path: 'env/.env',
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+
+      ['babel-plugin-styled-components'],
+      ['@babel/plugin-transform-flow-strip-types'],
+      ['@babel/plugin-proposal-async-generator-functions'],
+      ['@babel/plugin-proposal-object-rest-spread'],
+      ['@babel/plugin-transform-runtime'],
+      ['@babel/plugin-proposal-decorators', {
+        legacy: true
+      }],
+      [
+        '@babel/plugin-proposal-class-properties',
+        {
+          loose: true,
+        },
+      ],
+      [
+        'babel-plugin-root-import',
+        {
+          rootPathPrefix: '~',
+          rootPathSuffix: 'src',
+        },
+      ],
+    ],
   };
 };
