cad7a7d5f5acb5f1e83a6963d570787223ab660e
Alexandre Carvalho <alexandre.carvalho@opah.com.br>
2022-09-03 16:05:56 -0300

    Creating card

--- src/views/home/components/Card/index.tsx
+++ src/views/home/components/Card/index.tsx
@@ -13,6 +13,24 @@ const Card: React.FC<CardProps> = ({ title }) => {
       <S.Title>
         <S.Label fsize={16}>{title}</S.Label>
       </S.Title>
+      <S.Content>
+        <S.Rows>
+          <S.Label fsize={18}>Atividade:</S.Label>
+          <S.Label fsize={18}>Caminhada</S.Label>
+        </S.Rows>
+        <S.Rows>
+          <S.Label fsize={18}>Data:</S.Label>
+          <S.Label fsize={18}>03/09/2022</S.Label>
+        </S.Rows>
+        <S.Rows>
+          <S.Label fsize={18}>Duração:</S.Label>
+          <S.Label fsize={18}>00:28:00</S.Label>
+        </S.Rows>
+        <S.Rows>
+          <S.Label fsize={18}>Rendimento:</S.Label>
+          <S.Label fsize={18}>Médio</S.Label>
+        </S.Rows>
+      </S.Content>
     </S.Container>
   );
 };
--- src/views/home/components/Card/styles.tsx
+++ src/views/home/components/Card/styles.tsx
@@ -6,8 +6,7 @@ export const Container = styled(View)`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 300px;
-  padding: 16px;
+  padding: 10px 16px;
   border-top-left-radius: 25px;
   border-top-right-radius: 25px;
   border-bottom-right-radius: 25px;
@@ -21,11 +20,29 @@ export const Title = styled(View)`
   flex-direction: row;
   align-items: center;
   width: 100%;
-  height: 25px;
-  background-color: aliceblue;
+  height: 30px;
+  margin-bottom: 10px;
 `;
 
 export const Label = styled(Text)<any>`
   margin-right: 3px;
   font-size: ${(props: any) => (props.fsize ? props.fsize + "px" : "11px")};
+`;
+
+export const Content = styled(View)`
+  display: flex;
+  flex-direction: column;
+  padding: 20px 0 10px 0;
+  width: 100%;
+  border-top-width: 0.5px;
+  border-color: ${theme.colors.grey.n02};
+`;
+
+export const Rows = styled(View)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  height: 30px;
+  margin-bottom: 15px;
 `;
